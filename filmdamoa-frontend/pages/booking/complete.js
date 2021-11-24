b720b615208acc26b7de81c667a05f031e2909e4
Asiatic Chipmunk <osh4589@gmail.com>
2021-11-24 17:32:10 +0900

    예매 완료 화면 디자인 구축

--- filmdamoa-frontend/pages/booking/complete.js
+++ filmdamoa-frontend/pages/booking/complete.js
@@ -1,12 +1,206 @@
+import { useEffect, useRef } from 'react';
 import Head from 'next/head';
+import styled, { css } from 'styled-components';
 import * as cookie from 'cookie';
 import { getDataInNodeJs } from '../../lib/http';
-import { StyledArticle } from '../../lib/styledComponents';
+import { StyledArticle, scrollable } from '../../lib/styledComponents';
 import { withLayout } from '../../components';
 import { wrapper } from '../../store/store';
 import { setAccessToken } from '../../store/user/action';
 
+const InfoWrapper = styled.div`
+  display: flex;
+  overflow-x: auto;
+  ${scrollable};
+
+  @media ${({ theme }) => theme.media.mobile} {
+    flex-direction: column;
+    overflow-x: visible;
+  }
+`
+
+const PosterBox = styled.div`
+  height: 23.375rem;
+  padding: 2.5rem 0;
+  text-align: center;
+  background-color: #3f3f3f;
+
+  > div {
+    width: 16.875rem;
+    margin-bottom: 1.625rem;
+
+    div {
+      &:first-child {
+        margin-bottom: 0.125rem;
+        color: silver;
+        font-size: ${({ theme }) => theme.fontSize.medium};
+      }
+
+      &:last-child {
+        color: #55b0ba;
+        font-size: ${({ theme }) => theme.fontSize.great};
+        font-weight: bold;
+      }
+    }
+
+    @media ${({ theme }) => theme.media.mobile} {
+      width: 100%;
+    }
+  }
+
+  img {
+    width: 9.375rem;
+    height: 13.5rem;
+    border: 0;
+  }
+`
+
+const listDotStyles = css`
+  &::before {
+    display: block;
+    width: 3px;
+    height: 3px;
+    content: '';
+    background-color: #aaa;
+    position: absolute;
+    top: 9px;
+    left: 0;
+  }
+`
+
+const DetailBox = styled.div`
+  height: 23.375rem;
+  padding: 2.5rem 3.75rem;
+  background-color: #323232;
+
+  div {
+    width: 50.625rem;
+    color: #55b0ba;
+    font-size: 1.375rem;
+  }
+
+  i {
+    font-size: 1.125em;
+  }
+
+  ul {
+    width: 50.625rem;
+    padding: 1.25rem 0;
+    margin: 0.875rem 0;
+    list-style: none;
+    border-top: 1px solid #434343;
+
+    li {
+      padding-left: 9.625rem;
+      margin: 0.25rem 0;
+      color: white;
+      position: relative;
+
+      &:last-child {
+        padding-left: 13.625rem;
+
+        strong {
+          color: #59bec9;
+          font-size: 1.25em;
+          position: absolute;
+          left: 9.625rem;
+        }
+      }
+
+      ${listDotStyles};
+    }
+  }
+
+  span {
+    width: 9rem;
+    color: silver;
+    position: absolute;
+    left: 0.625rem;
+  }
+
+  @media ${({ theme }) => theme.media.mobile} {
+    height: auto;
+    padding: 2.5rem 1.625rem;
+    overflow-x: auto;
+    ${scrollable};
+
+    div, ul {
+      width: 24.875rem;
+    }
+
+    ul {
+      position: relative;
+
+      &::after {
+        width: 1.625rem;
+        height: 1px;
+        content: '';
+        position: absolute;
+        top: 0;
+        left: 100%;
+      }
+    }
+  }
+`
+
+const GuideWrapper = styled.div`
+  padding: 1rem;
+  margin-top: 2rem;
+  border: 1px solid ${({ theme }) => theme.colors.greyLight};
+  border-radius: 0.375rem;
+  color: #434343;
+
+  div {
+    padding-left: 0.5rem;
+    position: relative;
+
+    ${listDotStyles};
+  }
+
+  ul {
+    padding: 0.375rem 0 0;
+    margin: 0;
+    list-style: none;
+
+    li {
+      padding-left: 0.625rem;
+      margin: 0.25rem 0;
+      position: relative;
+
+      &::before {
+        display: block;
+        width: 4px;
+        height: 1px;
+        content: '';
+        background-color: #323232;
+        position: absolute;
+        top: 10px;
+        left: 0;
+      }
+    }
+  }
+
+  @media ${({ theme }) => theme.media.mobile} {
+    margin-top: 0.75rem;
+  }
+`
+
 const Complete = ({ data }) => {
+  const scrollRef = useRef();
+  useEffect(() => {
+    const handleWheel = event => {
+      if (scrollRef.current.contains(event.target)) {
+        event.preventDefault();
+        scrollRef.current.scrollLeft += event.deltaY;
+      }
+    }
+    document.addEventListener('wheel', handleWheel, { passive: false });
+
+    return () => {
+      document.removeEventListener('wheel', handleWheel, { passive: false });
+    }
+  }, []);
+
   return (
     <>
       <Head>
@@ -14,15 +208,15 @@ const Complete = ({ data }) => {
         <meta name="description" content="예매 완료 페이지입니다." />
       </Head>
       <StyledArticle>
-        <div>
-          <div>
+        <InfoWrapper ref={scrollRef}>
+          <PosterBox>
             <div>
               <div>티켓 예매번호</div>
               <div>{data.merchantUid}</div>
             </div>
             <img src={`${data.posterThumbnail.slice(0, -8)}_316.jpg`} alt={data.movieName} />
-          </div>
-          <div>
+          </PosterBox>
+          <DetailBox>
             <div>
               <strong>예매가 완료되었습니다 <i>!</i></strong>
             </div>
@@ -53,9 +247,9 @@ const Complete = ({ data }) => {
                 원
               </li>
             </ul>
-          </div>
-        </div>
-        <div>
+          </DetailBox>
+        </InfoWrapper>
+        <GuideWrapper>
           <div>
             상영안내
             <ul>
@@ -64,7 +258,7 @@ const Complete = ({ data }) => {
               <li>상영시간 20분전까지 취소 가능하며, 캡쳐화면으로는 입장하실 수 없습니다.</li>
             </ul>
           </div>
-        </div>
+        </GuideWrapper>
       </StyledArticle>
     </>
   );
